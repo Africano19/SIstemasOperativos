f6f1d54c00099ff41ebe70e86e9abeee244e7bc5
Ruben Africano <81010248+Africano19@users.noreply.github.com>
2023-05-16 16:41:59 +0100

    update

--- Server/src/main/java/com/example/MultithreadedServer.java
+++ Server/src/main/java/com/example/MultithreadedServer.java
@@ -5,25 +5,18 @@ package com.example;
 import java.io.*;
 import java.net.*;
 
-// Importa o Logger e LoggerFactory
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 // Define a classe principal
 public class MultithreadedServer {
 
     // Método principal, que é o ponto de entrada do programa
     public static void main(String[] args) throws IOException {
-        // Define o Logger
-        Logger logger = LoggerFactory.getLogger(MultithreadedServer.class);
-
         // Define o número da porta
         int port = 8080;
 
         // Tenta criar um socket de servidor, que escuta por conexões TCP recebidas
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             // Informa que o servidor foi iniciado e está escutando na porta especificada
-            logger.info("Server is listening on port " + port);
+            System.out.println("Server is listening on port " + port);
 
             // Entra em um loop infinito para aceitar conexões contínuas
             while (true) {
@@ -34,22 +27,21 @@ public class MultithreadedServer {
                 InetAddress clientAddress = socket.getInetAddress();
                 
                 // Informa que um novo cliente foi conectado
-                logger.info("New client connected: " + clientAddress.getHostAddress());
+                
+                System.out.println("New client connected: " + clientAddress.getHostAddress());
                 
                 // Cria um novo thread do servidor para lidar com a conexão do cliente e o inicia
                 new ServerThread(socket).start();
             }
         } catch (IOException ex) {
-            // Registra qualquer erro que ocorrer com o servidor
-            logger.error("Server exception: " + ex.getMessage(), ex);
+            // Imprime qualquer erro que ocorrer com o servidor
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
         }
     }
     
     // Define a classe ServerThread, que é usada para lidar com as conexões do cliente
     static class ServerThread extends Thread {
-        // Define o Logger
-        Logger logger = LoggerFactory.getLogger(ServerThread.class);
-
         // Define o socket que será usado para se comunicar com o cliente
         private Socket socket;
 
@@ -99,43 +91,44 @@ public class MultithreadedServer {
                             // Determina o tipo MIME do arquivo
                             String mimeType = URLConnection.guessContentTypeFromStream(resourceStream);
                             
-                            // Lê o conteúdo do arquivo
-                            byte[] fileContent = resourceStream.readAllBytes();
+                                                        // Lê o conteúdo do arquivo
+                                                        byte[] fileContent = resourceStream.readAllBytes();
 
-                            // Envia uma resposta HTTP 200 OK ao cliente
-                            writer.println("HTTP/1.1 200 OK");
-                            // Define o tipo de conteúdo da resposta
-                            writer.println("Content-Type: " + mimeType);
-                            // Define o tamanho do conteúdo da resposta
-                            writer.println("Content-Length: " + fileContent.length);
-                            // Envia uma linha vazia para indicar o fim dos cabeçalhos da resposta
-                            writer.println();
+                                                        // Envia uma resposta HTTP 200 OK ao cliente
+                                                        writer.println("HTTP/1.1 200 OK");
+                                                        // Define o tipo de conteúdo da resposta
+                                                        writer.println("Content-Type: " + mimeType);
+                                                        // Define o tamanho do conteúdo da resposta
+                                                        writer.println("Content-Length: " + fileContent.length);
+                                                        // Envia uma linha vazia para indicar o fim dos cabeçalhos da resposta
+                                                        writer.println();
                             
-                            // Envia o conteúdo do arquivo
-                            output.write(fileContent);
-                            // Limpa o fluxo de saída
-                            output.flush();
-                        } else {
-                            // Se o arquivo não foi encontrado, envia uma resposta HTTP 404 Not Found
-                            writer.println("HTTP/1.1 404 Not Found");
-                            // Define o tipo de conteúdo da resposta
-                            writer.println("Content-Type: text/html");
-                            // Define que o tamanho do conteúdo da resposta é 0
-                            writer.println("Content-Length: 0");
-                            // Envia uma linha vazia para indicar o fim dos cabeçalhos da resposta
-                            writer.println();
-                        }
-                    }
-                }
-
-                // Fecha o socket
-                socket.close();
-
-            } catch (IOException ex) {
-                // Registra qualquer erro que ocorrer com o thread
-                logger.error("Server exception: " + ex.getMessage(), ex);
-            }
-        }
-    }
-}
-
+                                                        // Envia o conteúdo do arquivo
+                                                        output.write(fileContent);
+                                                        // Limpa o fluxo de saída
+                                                        output.flush();
+                                                    } else {
+                                                        // Se o arquivo não foi encontrado, envia uma resposta HTTP 404 Not Found
+                                                        writer.println("HTTP/1.1 404 Not Found");
+                                                        // Define o tipo de conteúdo da resposta
+                                                        writer.println("Content-Type: text/html");
+                                                        // Define que o tamanho do conteúdo da resposta é 0
+                                                        writer.println("Content-Length: 0");
+                                                        // Envia uma linha vazia para indicar o fim dos cabeçalhos da resposta
+                                                        writer.println();
+                                                    }
+                                                }
+                                            }
+                            
+                                            // Fecha o socket
+                                            socket.close();
+                            
+                                        } catch (IOException ex) {
+                                            // Imprime qualquer erro que ocorrer com o thread
+                                            System.out.println("Server exception: " + ex.getMessage());
+                                            ex.printStackTrace();
+                                        }
+                                    }
+                                }
+                            }
+                            
